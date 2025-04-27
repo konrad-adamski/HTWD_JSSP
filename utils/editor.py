6697e5a769e9976614ebe5427dfff85298a72a61
konrad-adamski <konrad.adamski.ka@gmail.com>
2025-04-27 04:55:04 +0200

    Pseudo-Simulation

--- utils/editor.py
+++ utils/editor.py
@@ -25,18 +25,17 @@ def separate_operation_by_day_limit(df_schedule: pd.DataFrame, day_limit_h: floa
 
 
 
-def get_jssp_from_schedule(df_schedule: pd.DataFrame) -> dict:
+def get_jssp_from_schedule(df_schedule: pd.DataFrame, duration_column: str = "Duration") -> dict:
     job_dict = {}
 
     df_schedule = df_schedule.copy()
     df_schedule["Machine"] = df_schedule["Machine"].str.extract(r"M(\d+)").astype(int)
-    df_schedule["Duration"] = df_schedule["Duration"].astype(int)
+    df_schedule[duration_column] = df_schedule[duration_column].astype(int)
 
-    for job_name, group in df_schedule.groupby("Job"):
-        group_sorted = group.sort_values("Start")  # technologische Reihenfolge erhalten
-        operations = group_sorted[["Machine", "Duration"]].values.tolist()
-
-        job_dict[job_name] = operations
+    for job, machine, duration in zip(df_schedule["Job"], df_schedule["Machine"], df_schedule[duration_column]):
+        if job not in job_dict:
+            job_dict[job] = []
+        job_dict[job].append([machine, duration])
 
     return job_dict
 
